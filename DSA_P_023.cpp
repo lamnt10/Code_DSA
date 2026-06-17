f935d56f10710a304724535d0a1079f60e3419a5
lamnt10 <hhhgghjj74@gmail.com>
2026-06-17 15:05:29 +0700

    ql

--- DSA_P_023.cpp
+++ DSA_P_023.cpp
@@ -1,50 +1,41 @@
-#include <iostream>
-#include <math.h>
+#include<bits/stdc++.h>
 using namespace std;
-int n,k;
-int prime[10000001];
-
-void sang(){
-	for(int i = 0; i <= 10000000; i++){
-		prime[i] = 1;
-	}
-	prime[0] = prime[1] = 0;
-	for(int i = 2; i <= sqrt(10000000); i++){
-		if(prime[i]){
-			for(int j = i * i; j <= 10000000; j += i){
-				prime[j] = 0;
-			}
-		}
-	}
-}
-int check[1001];
-int a[1001];
+int prime[1000001];
+int n,k,a[105];
 int cnt=0;
-void back_track(int i){
-    for(int j=a[i-1]+1;j<=n-k+i;j++){
-        if(!check[j]){
-            a[i]=j;
-            check[j]=1;
-            if(i==k){
-                cnt ++;
-                if(prime[cnt]){
-                    cout << cnt << ": ";
-                    for(int i=1;i<=k;i++){
-                        cout << a[i]<< " ";
-                    }
-                    cout << endl;
-                }
+void sang(){
+    prime[0]=0;prime[1]=0;
+    for(int i=2;i<=1000000;i++){
+        prime[i]=1;
+    }
+    for(int i=2;i<sqrt(1000000);i++){
+        if(prime[i]){
+            for(int j=i*i;j<=1000000;j+=i){
+                prime[j]=0;
             }
-            else back_track(i+1);
-          check[j]=0;
         }
-          
     }
 }
-
+void Try(int pos){
+    if(pos>k){
+        cnt++;
+        if(prime[cnt]){
+            cout << cnt << ": " ;
+            for(int i=1;i<=k;i++){
+                cout << a[i] << " ";
+            }
+            cout << endl;
+        }
+        return;
+    }
+    for(int i=a[pos-1]+1;i<=n-k+pos;i++){
+        a[pos]=i;
+        Try(pos+1);
+    }
+}
 int main(){
+    sang();
+    a[0]=0;
     cin >> n >> k;
-	sang();
-	back_track(1);
-	return 0;
+    Try(1);
 }
